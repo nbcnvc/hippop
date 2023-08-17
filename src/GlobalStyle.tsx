6134f51ac2460cf3d5bcbdf7435378748b85bc6a
조성록 <jsr4613@naver.com>
2023-08-17 16:09:57 +0900

    initial setting complete (#3)

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+body {
+    
+}
+
+`;
+
+export default GlobalStyle;
