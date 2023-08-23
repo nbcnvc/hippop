84f73fe03460d5c2aaddcf86a0864307451a17e4
조성록 <jsr4613@naver.com>
2023-08-23 18:02:28 +0900

    feat: 기간별 필터링, 인기있는 팝업스토어, 최신 팝업스토어 필터링 구현 완료 (#30)

--- src/components/search/SearchCalendar.tsx
+++ src/components/search/SearchCalendar.tsx
@@ -1,128 +1,91 @@
 import React, { useState } from 'react';
-// 라이브러리
-import Calendar1 from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import moment from 'moment';
-// props타입
 import { SearchCalendarProps } from '../../types/props';
-//스타일
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
 import { styled } from 'styled-components';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 
-const SearchCalendar = ({ storeData, onDateRangeChange }: SearchCalendarProps) => {
-  const [period, setPeriod] = useState([storeData[0].period_start, storeData[0].period_end]);
-  const onChangeCalendar = (date: any) => {
-    setPeriod([date, date]);
+const SearchCalendar = ({ onSearch }: SearchCalendarProps) => {
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [endDate, setEndDate] = useState<Date>(new Date());
+
+  const handleStartDateChange = (date: Date) => {
+    if (startDate < date) {
+      alert('종료일 보다 클 수는 없어요~ ');
+      setEndDate(date);
+      setStartDate(date);
+    } else {
+      setStartDate(date);
+    }
   };
 
-  const onChangePeriod = (date: Date) => {
-    // 여기서 선택한 날짜를 시작과 끝으로 설정하여 배열로 만들어 전달합니다.
-    onDateRangeChange([date, date]);
+  const handleEndDateChange = (date: Date) => {
+    if (startDate > date) {
+      alert('시작일 보다 작을 수는 없어요~.');
+    } else {
+      setEndDate(date);
+    }
+  };
+
+  const handleSearch = () => {
+    onSearch(startDate, endDate);
   };
 
   return (
-    <div>
-      <StyleCalendar
-        calendarType="US"
-        locale="ko-KO"
-        onChange={onChangeCalendar}
-        onClickDay={onChangePeriod}
-        selectRange={true}
-        value={[period[0], period[1]]}
-        formatDay={(locale, date) => moment(date).format('D')}
-      />
-    </div>
+    <Container>
+      {/* <div>기간별</div> */}
+      <StartDateBox>
+        <CalendarMonthOutlinedIcon />
+        <StyledDatePicker // DatePicker의 styled-component명
+          locale={ko} //한글
+          dateFormat="yyyy.MM.dd"
+          selected={startDate}
+          closeOnScroll={true} // 스크롤을 움직였을 때 자동으로 닫히도록 설정 기본값 false
+          onChange={handleStartDateChange}
+        />
+      </StartDateBox>
+
+      <EndDateBox>
+        <CalendarMonthOutlinedIcon />
+        <StyledDatePicker
+          locale={ko} //한글
+          dateFormat="yyyy.MM.dd"
+          selected={endDate}
+          closeOnScroll={true} // 스크롤을 움직였을 때 자동으로 닫히도록 설정 기본값 false
+          onChange={handleEndDateChange}
+        />
+        <button onClick={handleSearch}>검색</button>
+      </EndDateBox>
+    </Container>
   );
 };
 
 export default SearchCalendar;
 
-const StyleCalendar = styled(Calendar1)`
-  react-calendar {
-    width: 400px;
-    max-width: 100%;
-    background-color: #fff;
-    color: #222;
-    border-radius: 8px;
-    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.125em;
-  }
-  .react-calendar__navigation button {
-    color: #6f48eb;
-    min-width: 44px;
-    background: none;
-    font-size: 16px;
-    margin-top: 8px;
-  }
-  .react-calendar__navigation button:enabled:hover,
-  .react-calendar__navigation button:enabled:focus {
-    background-color: #f8f8fa;
-  }
-  .react-calendar__navigation button[disabled] {
-    background-color: #f0f0f0;
-  }
-  abbr[title] {
-    text-decoration: none;
-  }
-  /* .react-calendar__month-view__days__day--weekend {
- color: #d10000;
-} */
-  .react-calendar__tile:enabled:hover,
-  .react-calendar__tile:enabled:focus {
-    background: #f8f8fa;
-    color: #6f48eb;
-    border-radius: 6px;
-  }
-  .react-calendar__tile--now {
-    background: #6f48eb33;
-    border-radius: 6px;
-    font-weight: bold;
-    color: #6f48eb;
-  }
-  .react-calendar__tile--now:enabled:hover,
-  .react-calendar__tile--now:enabled:focus {
-    background: #6f48eb33;
-    border-radius: 6px;
-    font-weight: bold;
-    color: #6f48eb;
-  }
-  .react-calendar__tile--hasActive:enabled:hover,
-  .react-calendar__tile--hasActive:enabled:focus {
-    background: #f8f8fa;
-  }
-  .react-calendar__tile--active {
-    background: #6f48eb;
-    border-radius: 6px;
-    font-weight: bold;
-    color: white;
-  }
-  .react-calendar__tile--active:enabled:hover,
-  .react-calendar__tile--active:enabled:focus {
-    background: #6f48eb;
-    color: white;
-  }
-  .react-calendar--selectRange .react-calendar__tile--hover {
-    background-color: #f8f8fa;
-  }
-  .react-calendar__tile--range {
-    background: #e2e2ff;
-    color: #6f48eb;
-    border-radius: 0;
-  }
-  .react-calendar__tile--rangeStart {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    border-top-left-radius: 6px;
-    border-bottom-left-radius: 6px;
-    background: #6f48eb;
-    color: white;
-  }
-  .react-calendar__tile--rangeEnd {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-right-radius: 6px;
-    border-bottom-right-radius: 6px;
-    background: #6f48eb;
-    color: white;
-  }
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 25px;
+
+  margin-bottom: 150px;
 `;
+
+const StartDateBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+`;
+const EndDateBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+`;
+
+const StyledDatePicker = styled(DatePicker)``;
