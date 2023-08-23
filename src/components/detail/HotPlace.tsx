b1c0a25d501eee14e67bdfd9ce20644b45ee4958
Jang Hyejin <hyeee513@naver.com>
2023-08-23 18:20:48 +0900

    Feat custom marker (#48)

--- src/components/detail/HotPlace.tsx
+++ src/components/detail/HotPlace.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 // 타입
 import { HotPlaceProps } from '../../types/props';
 import { HotPlaceData } from '../../types/types';
-// api
 
 const HotPlace = ({ setCategory, setIsShow, hotPlaceData }: HotPlaceProps) => {
-  // const { data: hotPlaceImgData, isLoading, isError } = useQuery(['hotPlaceImg'], () => {hotPlaceData?.map((data) => )});
-
-  // const {
-  //   data: hotPlaceImgData,
-  //   isLoading,
-  //   isError
-  // } = useQuery(['hotPlaceImg'], () => fetchHotPlaceData(hotPlaceData[0].place_name));
-  // console.log(hotPlaceData);
-
-  // console.log(hotPlaceImgData);
-
   const handleHotPlaceCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
     const name = (e.target as HTMLButtonElement).name;
     setCategory(name);
@@ -24,13 +12,6 @@ const HotPlace = ({ setCategory, setIsShow, hotPlaceData }: HotPlaceProps) => {
   const handleShowMarker = () => {
     setIsShow((prev) => !prev);
   };
-
-  // if (isLoading) {
-  //   return <div>로딩중입니다 ...</div>;
-  // }
-  // if (isError) {
-  //   return <div>오류가 발생했습니다 ...</div>;
-  // }
 
   return (
     <div>
--- src/components/detail/StoreMap.tsx
+++ src/components/detail/StoreMap.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useRef, useState } from 'react';
 // 타입
 import { Geocoder, HotPlaceInfo, HotPlaceImage, HotPlaceData } from '../../types/types';
 import { StoreMapProps } from '../../types/props';
-// 컴포넌트
-import HotPlace from './HotPlace';
 // api
 import { Kakao } from '../../api/store';
+// 컴포넌트
+import HotPlace from './HotPlace';
+import NearbyStore from './NearbyStore';
 
 declare global {
   interface Window {
@@ -111,10 +112,20 @@ const StoreMap = ({ storeLocation }: StoreMapProps) => {
           });
         };
 
+        // 마커이미지의 주소입니다
+        const imageSrc = 'https://i.postimg.cc/pX2LSM9r/1.png';
+        // 마커이미지의 크기입니다
+        const imageSize = new kakao.maps.Size(70, 75);
+        // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+        const imageOption = { offset: new kakao.maps.Point(30, 60) };
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
         // 마커로 표시
         const marker = new kakao.maps.Marker({
           map: map,
-          position: coords
+          position: coords,
+          image: markerImage
         });
 
         // 인포윈도우로 장소에 대한 설명 표시
@@ -137,7 +148,7 @@ const StoreMap = ({ storeLocation }: StoreMapProps) => {
         map.setCenter(coords);
       }
     });
-  }, [category, isShow]);
+  }, [storeLocation, category, isShow]);
 
   return (
     <div>
@@ -149,6 +160,7 @@ const StoreMap = ({ storeLocation }: StoreMapProps) => {
           marginTop: '70px'
         }}
       />
+      <NearbyStore />
       {hotPlaceData && <HotPlace setCategory={setCategory} setIsShow={setIsShow} hotPlaceData={hotPlaceData} />}
     </div>
   );
