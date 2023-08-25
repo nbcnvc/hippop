3bcefcfb684d6591190594e00d46d68db1a78b61
nbcnvc <nbcnvc.dev@gmail.com>
2023-08-25 16:32:15 +0900

    Export `supabaseStorageUrl`

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabaseStorageUrl = process.env.REACT_APP_SUPABASE_STORAGE_URL as string;
