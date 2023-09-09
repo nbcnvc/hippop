23bbb235b98c49ab0cec4faaf7ff6f3527061209
nbcnvc <nbcnvc.dev@gmail.com>
2023-09-09 17:39:21 +0900

    Remove keen-slider

--- src/components/list/Card.tsx
+++ src/components/list/Card.tsx
@@ -16,19 +16,8 @@ function getRandomElement(arr: number[]) {
 }
 
 const Card = (props: CardProps) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-    created() {
-      setLoaded(true);
-    }
-  });
-
   const { title, images, location, period_start, period_end, isClosed } = props.store;
+
   const [isHovered, setIsHovered] = useState(false);
   const [cardHeight, setCardHeight] = useState<number>(0);
 
@@ -47,16 +36,8 @@ const Card = (props: CardProps) => {
   return (
     <>
       {isClosed ? (
-        <CardContainerClosed
-          ref={sliderRef}
-          className="keen-slider"
-          style={{ height: cardHeight }}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          {images.map((image) => (
-            <img src={`${supabaseStorageUrl}/${image}`} className="keen-slider__slide" key={image} />
-          ))}
+        <CardContainerClosed style={{ height: cardHeight }} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          <img src={`${supabaseStorageUrl}/${images[0]}`} className="keen-slider__slide" />
           <ClosedStoreInfo>
             <CLosed>CLOSED</CLosed>
           </ClosedStoreInfo>
@@ -69,35 +50,10 @@ const Card = (props: CardProps) => {
               <span>{`${period_start} ~ ${period_end}`}</span>
             </div>
           </StoreInfo>
-          {loaded && instanceRef.current && (
-            <>
-              <Arrow
-                left
-                onClick={() => {
-                  instanceRef.current?.prev();
-                }}
-                disabled={currentSlide === 0}
-              />
-              <Arrow
-                onClick={() => {
-                  instanceRef.current?.next();
-                }}
-                disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
-              />
-            </>
-          )}
         </CardContainerClosed>
       ) : (
-        <CardContainer
-          ref={sliderRef}
-          className="keen-slider"
-          style={{ height: cardHeight }}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          {images.map((image) => (
-            <img src={`${supabaseStorageUrl}/${image}`} className="keen-slider__slide" key={image} />
-          ))}
+        <CardContainer style={{ height: cardHeight }} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          <img src={`${supabaseStorageUrl}/${images[0]}`} />
           <StoreInfo className="store-info" style={{ opacity: isHovered ? 1 : 0, maxHeight: isHovered ? '100%' : '0' }}>
             <div className="info-wrap">
               <h2>{title}</h2>
@@ -105,23 +61,6 @@ const Card = (props: CardProps) => {
               <span>{`${period_start} ~ ${period_end}`}</span>
             </div>
           </StoreInfo>
-          {loaded && instanceRef.current && (
-            <>
-              <Arrow
-                left
-                onClick={() => {
-                  instanceRef.current?.prev();
-                }}
-                disabled={currentSlide === 0}
-              />
-              <Arrow
-                onClick={() => {
-                  instanceRef.current?.next();
-                }}
-                disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
-              />
-            </>
-          )}
         </CardContainer>
       )}
     </>
@@ -241,25 +180,25 @@ const StoreInfo = styled.div`
   }
 `;
 
-const Arrow = (props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) => {
-  const disabeld = props.disabled ? ' arrow--disabled' : '';
-  const zIndex = 4;
-
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    props.onClick(e);
-  };
-
-  return (
-    <svg
-      onClick={handleClick}
-      style={{ zIndex }}
-      className={`arrow ${props.left ? 'arrow--left' : 'arrow--right'} ${disabeld}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
-      {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
-    </svg>
-  );
-};
+// const Arrow = (props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) => {
+//   const disabeld = props.disabled ? ' arrow--disabled' : '';
+//   const zIndex = 4;
+//
+//   const handleClick = (e: any) => {
+//     e.preventDefault();
+//     props.onClick(e);
+//   };
+//
+//   return (
+//     <svg
+//       onClick={handleClick}
+//       style={{ zIndex }}
+//       className={`arrow ${props.left ? 'arrow--left' : 'arrow--right'} ${disabeld}`}
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 24 24"
+//     >
+//       {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
+//       {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
+//     </svg>
+//   );
+// };
