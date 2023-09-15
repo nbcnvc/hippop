2a7878e00119fed7bff03c2b3b9dde09504cf214
조성록 <jsr4613@naver.com>
2023-09-15 14:15:46 +0900

    feat : 스크롤이 내려가있는 상태로 다른 페이지로 이동 시 스크롤 초기화 로직 추가

--- src/pages/RDetail.tsx
+++ src/pages/RDetail.tsx
@@ -61,7 +61,11 @@ const RDetail = () => {
 
   useEffect(() => {
     return () => {
-      window.scrollTo(0, 0);
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
     };
   }, []);
 
